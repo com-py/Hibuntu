d2e21f27d5af70b0e409f1e20bc23ea6e3baa599
philoteer <cdac1234@gmail.com>
2021-07-18 16:49:34 +0900

    Update sdcard-bootstrap.sh

--- sdcard-bootstrap.sh
+++ sdcard-bootstrap.sh
@@ -8,6 +8,6 @@ cgpt create ${DEV}
 cgpt add -i 1 -t kernel -b 8192 -s 32768 -l Kernel -S 1 -T 5 -P 10 ${DEV}
 cgpt show ${DEV}
 
-rootfs_pos="`cgpt show ${DEV} | grep \"Sec GPT Table\" | awk '{print $1}'`"
+rootfs_pos="`cgpt show ${DEV} | grep \"Sec GPT table\" | awk '{print $1}'`"
 cgpt add -i 2 -t data -b 40960 -s `expr ${rootfs_pos} - 40960` -l Root ${DEV}
 partx -a ${DEV}
