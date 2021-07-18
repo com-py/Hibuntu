0c194074430566615fb0fa151df83867439b750e
philoteer <cdac1234@gmail.com>
2021-07-18 16:51:35 +0900

    Update sdcard-bootstrap.sh

--- sdcard-bootstrap.sh
+++ sdcard-bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+set -x
 DEV="/dev/mmcblk0"
 
 umount ${DEV}*
@@ -9,5 +9,6 @@ cgpt add -i 1 -t kernel -b 8192 -s 32768 -l Kernel -S 1 -T 5 -P 10 ${DEV}
 cgpt show ${DEV}
 
 rootfs_pos="`cgpt show ${DEV} | grep \"Sec GPT table\" | awk '{print $1}'`"
+echo $rootfs_pos
 cgpt add -i 2 -t data -b 40960 -s `expr ${rootfs_pos} - 40960` -l Root ${DEV}
 partx -a ${DEV}
